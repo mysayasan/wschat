390ad5d24a7493c5626c3f6706242523ad856789
Muhammad Adib <mysayasan@gmail.com>
2021-08-23 14:31:17 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mysayasan/wschat-go
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/mysayasan/helpers-go v0.0.0-20210528234243-1e46c56bee84
+// github.com/mysayasan/helpers-go v0.0.0
+)
+
+// replace github.com/mysayasan/helpers-go v0.0.0 => ../helpers-go
---
+++ hub.go
@@ -0,0 +1,76 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wschat
+
+import (
+	arrayHelper "github.com/mysayasan/helpers-go/array"
+)
+
+type Message struct {
+	Topic   string
+	Message []byte
+}
+
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	Clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	Message chan *Message
+
+	// Register requests from the clients.
+	Register chan *Client
+
+	// Unregister requests from clients.
+	Unregister chan *Client
+}
+
+// NewHub create new hub
+func NewHub() *Hub {
+	hub := &Hub{
+		// Message:  make(chan []byte),
+		Message:    make(chan *Message),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+	}
+
+	go hub.run()
+
+	return hub
+}
+
+// Run run hub
+func (h *Hub) Run() {
+	go h.run()
+}
+
+// Run run hub
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.Register:
+			h.Clients[client] = true
+		case client := <-h.Unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.Send)
+			}
+		case message := <-h.Message:
+			for client := range h.Clients {
+				if arrayHelper.Is.StringInSlice(message.Topic, client.Topics) {
+					select {
+					case client.Send <- message.Message:
+					default:
+						close(client.Send)
+						delete(h.Clients, client)
+					}
+				}
+			}
+		}
+	}
+}
